62b2d8a470a962ce78e41b04735bd44293988d16
Reuben <treuben90@yahoo.com>
2024-12-28 15:48:17 +0200

    created the css styles

--- app/components/SwitchNumberBtn/SwitchNumber.js
+++ app/components/SwitchNumberBtn/SwitchNumber.js
@@ -4,23 +4,23 @@ import { RiArrowDropUpLine } from "react-icons/ri";
 
 export default function SwitchNumberBtn() {
   return (
-    <div className="flex items-center gap-5">
-      <div className="h-[30px] w-[30px] rounded-[50px]">
+    <div className="flex w-[var(--first-panel-width)] items-center gap-5 bg-[var(--chat-panel-green-bg-color)]">
+      <div className="h-[40px] w-[40px] rounded-[50px]">
         <Image
           src="/images/usFlag.png"
           alt="US Flag"
-          width={30}
-          height={30}
+          width={40}
+          height={40}
           className="h-full w-full rounded-full object-cover"
         />
       </div>
-      <div>
-        <p className="text-sm font-bold">+1 8635433222</p>
-        <p className="text-sm">Switch Number</p>
-      </div>
-      <div>
+      <div className="flex items-start gap-2">
+        <div>
+          <p className="text-sm font-bold">+1 8635433222</p>
+          <p className="text-sm">Switch Number</p>
+        </div>
         <RiArrowDropDownLine />
-        <RiArrowDropUpLine />
+        {/* <RiArrowDropUpLine /> */}
       </div>
     </div>
   );
